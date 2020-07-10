625f3d956b6c84f3307fba5c1975d2b0b9bad94b
Rolando Ramos <rolandorramos@gmail.com>
2020-07-10 23:35:56 +0200

    Added flag to prevent use of the SignalR hub if the connection fails

--- src/Games/Games.Worker/Worker.cs
+++ src/Games/Games.Worker/Worker.cs
@@ -1,14 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Games.Entities;
 using Games.Services.Interfaces;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 
 namespace Games.Worker
 {
@@ -17,6 +15,7 @@ namespace Games.Worker
         private readonly IAsyncRepository<Play> _playRepository;
         private readonly ILogger<Worker> _logger;
         private readonly HubConnection _hubConnection;
+        private bool _isHubActive;
         private Timer _gameTimer;
 
         class GameTime
@@ -26,34 +25,56 @@ namespace Games.Worker
 
         public Worker(IAsyncRepository<Play> playRepository, ILogger<Worker> logger, IConfiguration config)
         {
-            _logger = logger;
-            _playRepository = playRepository;
+            try
+            {
+                _logger = logger;
+                _playRepository = playRepository;
 
-            string hubEndpoint = config["HubEndpoint"];
-            _hubConnection = new HubConnectionBuilder()
-                .WithUrl(hubEndpoint).Build();
+                string hubEndpoint = config["HubEndpoint"];
+                _hubConnection = new HubConnectionBuilder()
+                    .WithUrl(hubEndpoint).Build();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error occurred initializing the worker service");
+            }
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
             var gameTime = new GameTime();
-            _hubConnection.StartAsync().Wait();
-
             _gameTimer = new Timer(new TimerCallback(DoWork), gameTime, TimeSpan.Zero, TimeSpan.FromSeconds(1));
 
+            try
+            {
+                _hubConnection.StartAsync().Wait();
+                _isHubActive = true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred starting the SignalR Hub");
+                _isHubActive = false;
+            }
+            
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _hubConnection.StopAsync().Wait();
+            if (_isHubActive)
+            {
+                _hubConnection.StopAsync().Wait();
+            }
 
             return Task.CompletedTask;
         }
 
         public void Dispose()
         {
-            _hubConnection.DisposeAsync().Wait();
+            if (_isHubActive)
+            {
+                _hubConnection.DisposeAsync().Wait();
+            }
         }
 
         private async void DoWork(object state)
@@ -62,12 +83,17 @@ namespace Games.Worker
             var pastGameTime = gameState.Current;
             Interlocked.Decrement(ref gameState.Current);
 
-            var plays = await _playRepository.ListAsync(p => p.GameSecondsRemaining < pastGameTime && p.GameSecondsRemaining >= gameState.Current);
+            var plays = await _playRepository
+                .ListAsync(p => p.GameSecondsRemaining < pastGameTime && p.GameSecondsRemaining >= gameState.Current);
 
             foreach (var play in plays)
             {
                 _logger.LogInformation(play.Desc);
-                await _hubConnection.SendAsync("SendPlay", play.Desc);
+
+                if (_isHubActive)
+                {
+                    await _hubConnection.SendAsync("SendPlay", play.Desc);
+                }
             }
         }
     }
