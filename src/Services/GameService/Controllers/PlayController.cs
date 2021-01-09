4d50872fae89e985ba629705306225b0c06f3ce4
Rolando Ramos <rolandorramos@gmail.com>
2021-01-09 23:57:36 +0100

    Create PlayController.cs

---
+++ src/Services/GameService/Controllers/PlayController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Football.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Football.Services.GameService.Controllers
+{
+    [Route("api/football/plays")]
+    public class PlayController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public PlayController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("week/{weekId}/{start}/{end}")]
+        public async Task<ActionResult> GetGameById(int weekId, int start, int end)
+        {
+            ReadOnlyCollection<IPlay> plays = await _repository.GetPlaysByGameTime(weekId, start, end);
+
+            return Ok(plays);
+        }
+    }
+}
