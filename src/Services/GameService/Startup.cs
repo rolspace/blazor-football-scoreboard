6b7a06a04a2757c3155b6dc063d0e0912d4492e4
Roland <rolandorramos@gmail.com>
2022-05-03 23:40:19 +0200

    Removed unnecessary using statement

--- src/Services/GameService/Startup.cs
+++ src/Services/GameService/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 
 namespace Football.Services.GameService
