b541d2e3796b7129ba56c7f3eb1ee187c961b7f9
Roland <rolandorramos@gmail.com>
2022-05-05 22:52:50 +0200

    Removed unnecessary using statement

--- src/Scoreboard/Scoreboard.Server/Startup.cs
+++ src/Scoreboard/Scoreboard.Server/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Scoreboard.Server.Hubs;
 
 namespace Scoreboard.Server
 {
