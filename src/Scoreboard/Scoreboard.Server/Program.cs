ffbe9a2af7ed465b0a4e186b212c5e01762693dc
Roland <rolandorramos@gmail.com>
2022-06-23 00:40:35 +0200

    Removed unnecessary comment and whitespace

--- src/Scoreboard/Scoreboard.Server/Program.cs
+++ src/Scoreboard/Scoreboard.Server/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.ResponseCompression;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -17,7 +16,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
