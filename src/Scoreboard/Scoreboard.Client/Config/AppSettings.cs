51a66d85c1d20ba17eadbdc354f9f59cf35ed111
Roland <rolandorramos@gmail.com>
2022-07-02 23:57:28 +0200

    Updated AppSettings class

--- src/Scoreboard/Scoreboard.Client/Config/AppSettings.cs
+++ src/Scoreboard/Scoreboard.Client/Config/AppSettings.cs
@@ -2,6 +2,10 @@ namespace Scoreboard.Client.Config
 {
     public class AppSettings
     {
+        public int GameWeek { get; set; }
+
+        public string? ApiEndpoint { get; set; }
+
         public string? HubEndpoint { get; set; }
     }
 }
