888acbd8c5d1ff1e32295e8be35b581ece093b8b
Roland <rolandorramos@gmail.com>
2022-07-15 23:07:34 +0200

    Removed file for custom play entity
    Moved everything to the entity class file

--- src/Core/Persistence/Entities/PlayEntity.cs
+++ src/Core/Persistence/Entities/PlayEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace Football.Core.Persistence.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Football.Core.Persistence.Entities
 {
     public partial class PlayEntity
     {
@@ -307,5 +309,53 @@
         public bool? DefensiveExtraPointConv { get; set; }
 
         public virtual GameEntity Game { get; set; }
+
+        [NotMapped]
+        protected internal bool IsHomeTeamOnOffense => Posteam == HomeTeam;
+
+        [NotMapped]
+        protected internal bool IsAwayTeamOnOffense => Posteam == AwayTeam;
+
+        [NotMapped]
+        protected internal bool IsHomeTeamReceivingKickoffOrPunt
+        {
+            get
+            {
+                bool isHomeTeamReceivingKickoff = PlayType == "kickoff" && Posteam == HomeTeam;
+                bool isHomeTeamReceivingPunt = PlayType == "punt" && Defteam == HomeTeam;
+
+                return isHomeTeamReceivingKickoff || isHomeTeamReceivingPunt;
+            }
+        }
+
+        [NotMapped]
+        protected internal bool IsAwayTeamReceivingKickoffOrPunt
+        {
+            get
+            {
+                bool isAwayTeamReceivingKickoff = PlayType == "kickoff" && Posteam == AwayTeam;
+                bool isAwayTeamReceivingPunt = PlayType == "punt" && Defteam == AwayTeam;
+
+                return isAwayTeamReceivingKickoff || isAwayTeamReceivingPunt;
+            }
+        }
+
+        [NotMapped]
+        protected internal bool IsHomeTeamPunting
+        {
+            get
+            {
+                return PlayType == "punt" && Posteam == HomeTeam;
+            }
+        }
+
+        [NotMapped]
+        protected internal bool IsAwayTeamPunting
+        {
+            get
+            {
+                return PlayType == "punt" && Posteam == AwayTeam;
+            }
+        }
     }
 }
