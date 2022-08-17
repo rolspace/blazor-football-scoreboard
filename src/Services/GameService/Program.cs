c94f6134f75de9b00a7ab3d6de47e5fec11565ca
Roland <rolandorramos@gmail.com>
2022-08-17 23:47:06 +0200

    Modified Game service to use serilog

--- src/Services/GameService/Program.cs
+++ src/Services/GameService/Program.cs
@@ -10,48 +10,73 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var mySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 28));
-
-builder.Services.AddDbContext<FootballDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("FootballDbContext"), mySqlServerVersion));
-
-builder.Services.AddScoped<IFootballDataProvider, MySqlFootballDataProvider>();
-builder.Services.AddSignalR();
-builder.Services.AddControllers();
-builder.Services.AddHealthChecks();
-builder.Services.AddSwaggerGen(c =>
+try
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Service.Game", Version = "v1" });
-});
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-WebApplication app = builder.Build();
+    builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) => loggerConfiguration
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(hostBuilderContext.Configuration));
 
-if (builder.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service.Game v1"));
+    var mySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 28));
+
+    builder.Services.AddDbContext<FootballDbContext>(options =>
+        options.UseMySql(builder.Configuration.GetConnectionString("FootballDbContext"), mySqlServerVersion));
+
+    builder.Services.AddScoped<IFootballDataProvider, MySqlFootballDataProvider>();
+    builder.Services.AddSignalR();
+    builder.Services.AddControllers();
+    builder.Services.AddHealthChecks();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Service.Game", Version = "v1" });
+    });
+
+    WebApplication app = builder.Build();
+
+    if (builder.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service.Game v1"));
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseCors(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.WithOrigins(builder.Configuration["AllowedOrigin"])
+            .AllowAnyHeader()
+            .WithMethods("GET", "POST")
+            .AllowCredentials();
+    });
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapHealthChecks("/health");
+        endpoints.MapHub<GameHub>("/hub/football/game");
+        endpoints.MapControllers();
+    });
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseCors(corsPolicyBuilder =>
+catch (Exception ex)
 {
-    corsPolicyBuilder.WithOrigins(builder.Configuration["AllowedOrigin"])
-        .AllowAnyHeader()
-        .WithMethods("GET", "POST")
-        .AllowCredentials();
-});
-app.UseAuthorization();
+    Log.Fatal(ex, "Application startup failed");
 
-app.UseEndpoints(endpoints =>
+}
+finally
 {
-    endpoints.MapHealthChecks("/health");
-    endpoints.MapHub<GameHub>("/hub/football/game");
-    endpoints.MapControllers();
-});
-
-app.Run();
+    Log.Information("Application shut down complete");
+    Log.CloseAndFlush();
+}
