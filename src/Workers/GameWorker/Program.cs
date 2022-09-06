cedaa1ada7447829660c65be1dd14cab0d11bdcf
Roland <rolandorramos@gmail.com>
2022-09-06 23:08:00 +0200

    Converted game work startup to use minimal api

--- src/Workers/GameWorker/Program.cs
+++ src/Workers/GameWorker/Program.cs
@@ -2,31 +2,23 @@ using System;
 using Football.Core.Persistence.Interfaces.DataProviders;
 using Football.Core.Persistence.MySql;
 using Football.Core.Persistence.MySql.Contexts;
+using Football.Workers.GameWorker;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Football.Workers.GameWorker
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices((hostContext, services) =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    services.AddHostedService<Worker>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
+    var mySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 28));
+    services.AddDbContext<FootballDbContext>(options =>
+        options.UseMySql(hostContext.Configuration.GetConnectionString("FootballDbContext"), mySqlServerVersion));
 
-                    var mySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 28));
-                    services.AddDbContext<FootballDbContext>(options =>
-                        options.UseMySql(hostContext.Configuration.GetConnectionString("FootballDbContext"), mySqlServerVersion));
+    services.AddScoped<IFootballDataProvider, MySqlFootballDataProvider>();
+});
 
-                    services.AddScoped<IFootballDataProvider, MySqlFootballDataProvider>();
-                });
-    }
-}
+builder.Build().Run();
