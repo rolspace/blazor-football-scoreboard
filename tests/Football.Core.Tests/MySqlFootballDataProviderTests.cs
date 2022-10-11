c6965468875cbbd77fe197ff84467a154c2992f2
Roland <rolandorramos@gmail.com>
2022-10-11 23:40:28 +0200

    Created first test for MySqlFootballDataProvider

--- tests/Football.Core.Tests/MySqlFootballDataProviderTests.cs
+++ tests/Football.Core.Tests/MySqlFootballDataProviderTests.cs
@@ -1,10 +1,24 @@
+using Football.Core.Models;
+using Football.Core.Persistence.MySql;
+
 namespace Football.Core.Tests;
 
-public class MySqlFootballDataProviderTests
+public class MySqlFootballDataProviderTests : IClassFixture<TestDatabaseFixture>
 {
-    [Fact]
-    public void Test1()
-    {
+    public TestDatabaseFixture Fixture { get; }
 
+    public MySqlFootballDataProviderTests(TestDatabaseFixture testDatabaseFixture)
+        => Fixture = testDatabaseFixture;
+
+    [Fact]
+    public async Task GetGame_IdDoesNotExist_ReturnsNull()
+    {
+        using var dbContext = Fixture.CreateContext();
+
+        MySqlFootballDataProvider mySqlFootballDataProvider = new MySqlFootballDataProvider(dbContext);
+
+        Game game = await mySqlFootballDataProvider.GetGame(0);
+
+        Assert.Null(game);
     }
 }
