b8df0a8069ec3c255834c69b04f93036175cab75
Roland <rolandorramos@gmail.com>
2022-11-28 23:40:06 +0100

    Correct issue with namespace

--- src/Application/Common/Mappings/MappingProfile.cs
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using AutoMapper;
 
-namespace CleanArchitecture.Application.Common.Mappings;
+namespace Football.Application.Common.Mappings;
 
 public class MappingProfile : Profile
 {
