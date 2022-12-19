7c343ce9fe12d76b4c54aaf36839530e897381d8
Roland <rolandorramos@gmail.com>
2022-12-19 23:47:35 +0100

    Changed query and removed unnecessary file

--- src/Application/Games/Queries/GetGames/GamesVm.cs
+++
@@ -1,8 +0,0 @@
-using Football.Application.Common.Models;
-
-namespace Football.Application.Games.Queries;
-
-public class GamesVm
-{
-    public IList<GameDto> Games { get; set; } = new List<GameDto>();
-}
--- src/Application/Games/Queries/GetGames/GetGamesQuery.cs
+++ src/Application/Games/Queries/GetGames/GetGamesQuery.cs
@@ -1,12 +1,22 @@
+
+using Football.Application.Common.Interfaces;
+using Football.Application.Common.Models;
 using MediatR;
 
 namespace Football.Application.Games.Queries.GetGames;
 
-public record GetGamesQuery : IRequest<GamesVm>;
+public record GetGamesQuery : IRequest<IEnumerable<GameDto>>;
 
-public class GetGamesQueryHandler : IRequestHandler<GetGamesQuery, GamesVm>
+public class GetGamesQueryHandler : IRequestHandler<GetGamesQuery, IEnumerable<GameDto>>
 {
-    public Task<GamesVm> Handle(GetGamesQuery query, CancellationToken cancellationToken)
+    private IFootballDbContext _footballDbContext;
+
+    public GetGamesQueryHandler(IFootballDbContext footballDbContext)
+    {
+        _footballDbContext = footballDbContext;
+    }
+
+    public Task<IEnumerable<GameDto>> Handle(GetGamesQuery query, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
