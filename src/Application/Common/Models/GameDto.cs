cd93b654d44bff530e8d4f1a22410f06942bdd16
Roland <rolandorramos@gmail.com>
2022-12-22 23:35:08 +0100

    Add necessary properties to GameDto

--- src/Application/Common/Models/GameDto.cs
+++ src/Application/Common/Models/GameDto.cs
@@ -5,5 +5,11 @@ namespace Football.Application.Common.Models;
 
 public class GameDto : IMapFrom<Game>
 {
+    public int Id { get; set; }
 
+    public int Week { get; set; }
+
+    public string HomeTeam { get; set; } = string.Empty;
+
+    public string AwayTeam { get; set; } = string.Empty;
 }
