6ef64062b0875b475a133c10a3d05dd5a876b2b1
Roland <rolandorramos@gmail.com>
2022-12-25 22:11:11 +0100

    Add service configuration class

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
