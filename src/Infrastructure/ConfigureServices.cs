ebc43f32d45733e9b76292b77b6bcd5bfd5d1c46
Roland <rolandorramos@gmail.com>
2023-01-07 22:46:24 +0100

    Added references to Application and Infrastructure projects

---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,24 @@
+using Football.Application.Common.Interfaces;
+using Football.Infrastructure.Persistence;
+using Football.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var mySqlServerVersion = new MySqlServerVersion(new Version(8, 0, 28));
+
+        services.AddDbContext<FootballDbContext>(options =>
+            options.UseMySql(configuration.GetConnectionString("FootballDbConnection"), mySqlServerVersion));
+
+        services.AddScoped<IFootballDbContext>(provider => provider.GetRequiredService<FootballDbContext>());
+
+        services.AddTransient<IDateTime, DateTimeService>();
+
+        return services;
+    }
+}
