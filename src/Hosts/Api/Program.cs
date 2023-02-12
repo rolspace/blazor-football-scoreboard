c301a318eaacec20949ce405165ccc7f1770e4c3
Roland <rolandorramos@gmail.com>
2023-02-12 21:30:41 +0100

    Included Serilog app startup code

--- src/Hosts/Api/Program.cs
+++ src/Hosts/Api/Program.cs
@@ -1,22 +1,53 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Events;
 
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Began Application startup");
+
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(hostBuilderContext.Configuration)
+        .Enrich.FromLogContext()
+    );
+
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+
+    app.MapControllers();
+
+    Log.Information("Completed application startup");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Failed application startup");
+}
+finally
+{
+    Log.Information("Completed application shut down");
+    Log.CloseAndFlush();
+}
