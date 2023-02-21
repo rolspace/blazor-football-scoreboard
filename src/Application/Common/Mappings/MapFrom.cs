3d032ae32dc8b9be7c346da48aad86fc01001c9a
Roland <rolandorramos@gmail.com>
2023-02-21 22:21:14 +0100

    Changed interface to abstract class

--- src/Application/Common/Mappings/MapFrom.cs
+++ src/Application/Common/Mappings/MapFrom.cs
@@ -3,7 +3,7 @@ using AutoMapper;
 namespace Football.Application.Common.Mappings;
 
 //TODO: change to something else, I do not like this interface with a default method. Looks dirty.
-public interface IMapFrom<T>
+public abstract class MapFrom<T>
 {
-    void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    public void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
 }
--- src/Application/Common/Models/GameDto.cs
+++ src/Application/Common/Models/GameDto.cs
@@ -3,7 +3,7 @@ using Football.Domain.Entities;
 
 namespace Football.Application.Common.Models;
 
-public class GameDto : IMapFrom<Game>
+public class GameDto : MapFrom<Game>
 {
     public int Id { get; set; }
 
