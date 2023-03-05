f22f49318f4286001070cdfa346ec74cdddb0576
Roland <rolandorramos@gmail.com>
2023-03-05 10:09:06 +0100

    Refactored usage of hub connection

--- src/Hosts/Worker/PlayLogHostedService.cs
+++ src/Hosts/Worker/PlayLogHostedService.cs
@@ -1,26 +1,47 @@
+using Football.Worker.Providers;
+
 namespace Football.Worker;
 
 public class PlayLogHostedService : IHostedService, IAsyncDisposable
 {
+    private readonly IHubProvider _hubProvider;
+
     private readonly ILogger<PlayLogHostedService> _logger;
 
-    public PlayLogHostedService(ILogger<PlayLogHostedService> logger)
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public PlayLogHostedService(IHubProvider hubProvider, IServiceScopeFactory scopeFactory, ILogger<PlayLogHostedService> logger)
     {
+        _hubProvider = hubProvider;
+        _scopeFactory = scopeFactory;
         _logger = logger;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("Service started");
+
+        try
+        {
+            await _hubProvider.StartHubAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred starting the SignalR connection hub");
+        }
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("Service stopped");
+
+        await _hubProvider.StopHubAsync();
     }
 
     public async ValueTask DisposeAsync()
     {
         _logger.LogInformation("Service disposed");
+
+        await _hubProvider.DisposeHubAsync();
     }
 }
--- src/Hosts/Worker/Program.cs
+++ src/Hosts/Worker/Program.cs
@@ -1,6 +1,16 @@
 using Football.Worker;
+using Football.Worker.Providers;
+using Football.Worker.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<IHubProvider>((serviceProvider) =>
+{
+    var hubSettings = builder.Configuration.GetSection(HubSettings.Hub).Get<HubSettings>();
+    var hubUri = new Uri(hubSettings.Endpoint);
+
+    return new HubProvider(hubUri);
+});
 builder.Services.AddHostedService<PlayLogHostedService>();
 
 var app = builder.Build();
