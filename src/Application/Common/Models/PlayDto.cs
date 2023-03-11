a1213060f892da1ea9d7a0ca5e1466503a218dea
Roland <rolandorramos@gmail.com>
2023-03-11 15:45:53 +0100

    First version of DTO for play object

--- src/Application/Common/Models/PlayDto.cs
+++ src/Application/Common/Models/PlayDto.cs
@@ -1,9 +1,30 @@
+using AutoMapper;
 using Football.Application.Common.Mappings;
 using Football.Domain.Entities;
 
 namespace Football.Application.Common.Models;
 
-public class PlayDto : IMapFrom<Play>
+public class PlayDto : MapFrom<Play>
 {
+    public int Id { get; set; }
 
+    public string Description { get; set; } = string.Empty;
+
+    public int HomeScore { get; set; }
+
+    public int AwayScore { get; set; }
+
+    public int Quarter { get; set; }
+
+    public int QuarterSecondsRemaining { get; set; }
+
+    protected new void Mapping(Profile profile)
+    {
+        profile.CreateMap<Play, PlayDto>()
+            .ForMember(d => d.Id, o => o.MapFrom(s => s.PlayId))
+            .ForMember(d => d.Description, o => o.MapFrom(s => s.Desc))
+            .ForMember(d => d.HomeScore, o => o.MapFrom(s => s.TotalHomeScore))
+            .ForMember(d => d.AwayScore, o => o.MapFrom(s => s.TotalAwayScore))
+            .ForMember(d => d.Quarter, o => o.MapFrom(s => s.Qtr));
+    }
 }
