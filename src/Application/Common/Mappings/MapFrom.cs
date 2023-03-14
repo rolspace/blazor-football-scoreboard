188e006f3809b48b7a2fd2b7f3b33c321d132aeb
Roland <rolandorramos@gmail.com>
2023-03-14 10:38:25 +0100

    Made method in abstract class virtual

--- src/Application/Common/Mappings/MapFrom.cs
+++ src/Application/Common/Mappings/MapFrom.cs
@@ -5,5 +5,5 @@ namespace Football.Application.Common.Mappings;
 //TODO: change to something else, I do not like this interface with a default method. Looks dirty.
 public abstract class MapFrom<T>
 {
-    public void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    public virtual void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
 }
