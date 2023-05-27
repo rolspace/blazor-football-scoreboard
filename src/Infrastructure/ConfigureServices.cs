20eeb72beba49ec0dec8bebc42a0527b2dea578c
Roland <rolandorramos@gmail.com>
2023-05-27 09:47:02 +0200

    Added service conditional loading

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -17,14 +17,15 @@ public static class ConfigureServices
 
         services.AddScoped<IFootballDbContext>(provider => provider.GetRequiredService<FootballDbContext>());
 
-        services.AddTransient<IHubManager>((serviceProvider) =>
+        HubSettings? hubSettings = configuration.GetSection(HubSettings.HubSection).Get<HubSettings>();
+        if (hubSettings is not null)
         {
-            var hubSettings = configuration.GetSection(HubSettings.HubSection).Get<HubSettings>();
-
-            // TODO: review this later, Hub initialization is not needed everywhere
-            var hubUri = new Uri(hubSettings != null ? hubSettings.HubUrl : string.Empty);
-            return new HubManager(hubUri);
-        });
+            services.AddTransient<IHubManager>((serviceProvider) =>
+            {
+                var hubUri = new Uri(hubSettings.HubUrl);
+                return new HubManager(hubUri);
+            });
+        }
 
         return services;
     }
