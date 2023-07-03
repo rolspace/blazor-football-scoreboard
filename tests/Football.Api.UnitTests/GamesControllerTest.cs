3e8f5d4f5614160bc73fe2bbbc5cc609d7fcbb22
Roland <10880743+rolspace@users.noreply.github.com>
2023-07-03 21:59:08 +0200

    Added one more test for controller

--- tests/Football.Api.UnitTests/GamesControllerTest.cs
+++ tests/Football.Api.UnitTests/GamesControllerTest.cs
@@ -1,6 +1,8 @@
 using Football.Api.Controllers.V1;
 using Football.Application.Features.Games;
 using Football.Application.Features.Games.Models;
+using Football.Application.Features.Stats;
+using Football.Application.Features.Stats.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -120,5 +122,47 @@ public class GamesControllerTest
         var actionResult = await gamesController.GetGamesByWeek(getGamesQuery);
 
         var notFoundResult = Assert.IsType<BadRequestResult>(actionResult.Result);
+    }
+
+    [Fact]
+    public async Task GetStatsById_ValidGameId_ReturnsGameStatsDtoModel()
+    {
+        var getGameStatsQuery = new GetGameStatsQuery() { Id = 1 };
+        var gameStatDto = new GameStatDto
+        {
+            GameId = 1,
+            Stats = new List<StatDto> {
+                new StatDto
+                {
+                    Team = "HTM",
+                    Home = true,
+                    Score = 7,
+                    PassingYards = 100,
+                    Sacks = 3,
+                    ReturnYards = 20,
+                    Punts = 2
+                },
+                new StatDto
+                {
+                    Team = "ATM",
+                    Home = false,
+                    Score = 0,
+                    PassingYards = 50,
+                    Sacks = 1,
+                    ReturnYards = 10,
+                    Punts = 4
+                }
+            }
+        };
+
+        var mockSender = new Mock<ISender>();
+        mockSender.Setup(sender => sender.Send(getGameStatsQuery, new CancellationToken()))
+            .ReturnsAsync(gameStatDto);
+
+        var gamesController = new GamesController(mockSender.Object);
+        var actionResult = await gamesController.GetStatsById(getGameStatsQuery);
+
+        var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        Assert.Equal(gameStatDto, okResult.Value);
     }
 }
