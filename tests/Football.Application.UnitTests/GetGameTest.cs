323fc68d470827f92657cff1c4ba3a69f16fc44a
Roland <10880743+rolspace@users.noreply.github.com>
2023-07-13 00:37:03 +0200

    Create first unit tests for application tests

---
+++ tests/Football.Application.UnitTests/GetGameTest.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using FluentAssertions;
+using Football.Application.Features.Games;
+using Football.Application.Features.Games.Mappings;
+using Football.Application.Features.Games.Models;
+using Football.Infrastructure.Persistence;
+
+namespace Football.Application;
+
+public class GetGameTest : IClassFixture<TestDatabaseFixture>
+{
+    private readonly IMapper _mapper;
+
+    public TestDatabaseFixture Fixture { get; }
+
+    public GetGameTest(TestDatabaseFixture fixture)
+    {
+        Fixture = fixture;
+
+        if (_mapper is null)
+        {
+            MapperConfiguration mappingConfig = new(mapperConfiguration =>
+            {
+                mapperConfiguration.AddProfile(new MapFromEntityProfile());
+            });
+
+            _mapper = mappingConfig.CreateMapper();
+        }
+    }
+
+    [Fact]
+    public async Task GetGame_IdExists_ReturnsGame()
+    {
+        using FootballDbContext dbContext = Fixture.CreateContext();
+
+        GameDto expected = new()
+        {
+            Id = 2019090500,
+            Week = 1,
+            HomeTeam = "CHI",
+            HomeScore = 0,
+            AwayTeam = "GB",
+            AwayScore = 0,
+            State = string.Empty,
+            Quarter = 1,
+            QuarterSecondsRemaining = 900
+        };
+
+        GetGameQueryHandler handler = new(dbContext, _mapper);
+
+        GameDto? result = await handler.Handle(new GetGameQuery
+        {
+            Id = 2019090500
+        }, new CancellationToken());
+
+        result.Should().BeEquivalentTo(expected);
+    }
+}
+
