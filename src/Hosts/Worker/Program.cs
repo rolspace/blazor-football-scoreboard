cc4c254721c070a13854aab17d3bea69a5290238
Roland <10880743+rolspace@users.noreply.github.com>
2023-09-08 22:50:36 +0200

    Changed logging to use Serilog in worker

--- src/Hosts/Worker/Program.cs
+++ src/Hosts/Worker/Program.cs
@@ -1,34 +1,62 @@
 using Football.Infrastructure.Extensions;
 using Football.Worker;
+using Serilog;
+using Serilog.Events;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-if (builder.Environment.IsLocalhost())
+try
 {
-    builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+    Log.Information("Application start.");
+
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+    if (builder.Environment.IsLocalhost())
+    {
+        builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+    }
+
+    builder.Host.UseDefaultServiceProvider(options =>
+    {
+        bool validate = builder.Environment.IsDevelopment() || builder.Environment.IsLocalhost();
+        options.ValidateScopes = validate;
+        options.ValidateOnBuild = validate;
+    });
+
+    builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(hostBuilderContext.Configuration)
+        .Enrich.FromLogContext()
+    );
+
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+
+    builder.Services.AddHostedService<PlayLogBackgroundService>();
+
+    var app = builder.Build();
+
+    app.MapGet("/", () => new Response
+    {
+        Message = "Web application for the PlayLogHostedService"
+    });
+
+    app.Run();
 }
-
-builder.Host.UseDefaultServiceProvider(options =>
+catch (Exception ex)
 {
-    bool validate = builder.Environment.IsDevelopment() || builder.Environment.IsLocalhost();
-    options.ValidateScopes = validate;
-    options.ValidateOnBuild = validate;
-});
-
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-
-builder.Services.AddHostedService<PlayLogBackgroundService>();
-
-var app = builder.Build();
-
-app.MapGet("/", () => new Response
+    Log.Fatal(ex, "Application terminated unexpectedly.");
+}
+finally
 {
-    Message = "Web application for the PlayLogHostedService"
-});
-
-app.Run();
+    Log.Information("Application shut down.");
+    Log.CloseAndFlush();
+}
 
 public class Response
 {
