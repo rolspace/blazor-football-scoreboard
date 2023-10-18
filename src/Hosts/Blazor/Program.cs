8ce9390c1969ef11457308d90fd2be1b2e56de9e
Roland <10880743+rolspace@users.noreply.github.com>
2023-10-18 23:53:29 +0200

    Add Serilog browser sink to Blazor startup

--- src/Hosts/Blazor/Program.cs
+++ src/Hosts/Blazor/Program.cs
@@ -2,17 +2,33 @@ using Football.Blazor;
 using Football.Blazor.Settings;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.BrowserConsole()
+                .CreateLogger();
 
-var scoreboardSettings = builder.Configuration
-    .GetSection(ScoreboardSettings.Key)
-    .Get<ScoreboardSettings>();
+try
+{
+    Log.Information("Blazor host application start.");
 
-builder.Services.AddSingleton(scoreboardSettings);
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    var scoreboardSettings = builder.Configuration
+        .GetSection(ScoreboardSettings.Key)
+        .Get<ScoreboardSettings>();
 
-await builder.Build().RunAsync();
+    builder.Services.AddSingleton(scoreboardSettings);
+
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+    await builder.Build().RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Blazor host application terminated unexpectedly.");
+    throw;
+}
