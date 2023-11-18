fbc0357814d0efa6d626f9e6fd4f5c8c1e26e58c
Roland <10880743+rolspace@users.noreply.github.com>
2023-11-18 22:40:35 +0100

    Fixed rule to more accurately represent bounds

--- src/Application/Features/Games/GetGamesQueryValidator.cs
+++ src/Application/Features/Games/GetGamesQueryValidator.cs
@@ -7,7 +7,7 @@ public class GetGamesQueryValidator : AbstractValidator<GetGamesQuery>
     public GetGamesQueryValidator()
     {
         RuleFor(g => g.Week)
-            .Must(week => week > 0 && week < 18)
+            .Must(week => week >= 1 && week <= 17)
             .WithMessage("Week must be between 1 and 17");
     }
 }
