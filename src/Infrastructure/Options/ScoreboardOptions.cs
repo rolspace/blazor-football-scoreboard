f977bd87c7db09f87bee8b1df79894cce4eae819
Roland <10880743+rolspace@users.noreply.github.com>
2023-12-10 01:48:37 +0100

    Corrected property description

--- src/Infrastructure/Options/ScoreboardOptions.cs
+++ src/Infrastructure/Options/ScoreboardOptions.cs
@@ -5,7 +5,7 @@ public class ScoreboardOptions
     public const string Key = "Scoreboard";
 
     /// <summary>
-    /// Number of the week for the games schedule
+    /// Week number for the games schedule
     /// Should be a number from 1 to 17
     /// </summary>
     public int Week { get; set; } = 1;
