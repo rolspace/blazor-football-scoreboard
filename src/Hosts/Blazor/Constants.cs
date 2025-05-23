1cdc5657eab3566a3587e7f21f4dac7f78207907
Roland <10880743+rolspace@users.noreply.github.com>
2025-05-23 23:16:00 +0200

    fix: Removed unnecessary constant

--- src/Hosts/Blazor/Constants.cs
+++ src/Hosts/Blazor/Constants.cs
@@ -3,7 +3,5 @@ namespace Football.Blazor
     public static class Constants
     {
         public const string DefaultHttpClient = "DefaultHttpClient";
-
-        public const string DefaultPipeline = "DefaultPipeline";
     }
 }
